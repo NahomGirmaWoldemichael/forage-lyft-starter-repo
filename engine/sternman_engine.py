e7c0695922e36d9329891d68fe42a47989c4bdc0
NahomGirmaWoldemichael <nahom.girma@aastustudent.edu.et>
2023-12-02 13:59:08 +0300

    refactored

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from abc import ABC
 
 from car import Car
@@ -10,6 +11,13 @@ class SternmanEngine(Car, ABC):
 
     def engine_should_be_serviced(self):
         if self.warning_light_is_on:
+            return True
+        else:
+            return False
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
             return True
         else:
             return False
